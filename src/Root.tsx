99b15c333c32063ef4aec48c89626389dc12ff59
Tyler Martin <t.mar92@gmail.com>
2024-12-13 23:41:15 -0500

    adds viewTransition to try and add cross fade between pages

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,12 +6,11 @@ function Root() {
   return (
     <>
       <nav>
-        <NavLink to="/">About</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink viewTransition to="/">About</NavLink>
+        <NavLink viewTransition to="/projects">Projects</NavLink>
+        <NavLink viewTransition to="/contact">Contact</NavLink>
       </nav>
       <main>
-        <div>main content outlet below</div>
         <Outlet />
       </main>
       <footer>&#169; 2024</footer>
