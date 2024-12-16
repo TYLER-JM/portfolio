516448a5c4982cb037a8fcbe50e3019eb9dcdad5
Tyler Martin <t.mar92@gmail.com>
2024-12-16 18:54:48 -0500

    adds icons for project page and contact page

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -6,13 +6,19 @@ const Contact = function() {
 			<div className="contact__container">
 				<div className='contact__social'>
 					<a className="social github" href="https://github.com/TYLER-JM" target="blank">
-						Github
+						<svg className="github">
+							<use xlinkHref="images/sprite.svg#icon-github"></use>
+						</svg>
 					</a>
 					<a className="social linkedin" href="https://www.linkedin.com/in/tyler-martin-670020198/" target="blank">
-						LinkedIn
+						<svg className="linkedin">
+							<use xlinkHref="images/sprite.svg#icon-linkedin-with-circle"></use>
+						</svg>
 					</a>
 					<a className="social mail" href="mailto:tyler.j.a.martin@gmail.com" target="_blank" rel="noopener noreferrer">
-						Email
+						<svg className="mail">
+							<use xlinkHref="images/sprite.svg#icon-mail"></use>
+						</svg>
 					</a>
 				</div>
 				<p>I'm always happy to chat about what I'm working on, what I'm reading, what I'm playing, or what I'm listening to.</p>
